01fbd186fa4a9e080152d7a625cf1d87adcd3882
Chris Runda <vegan@gmx.at>
2023-10-12 09:18:20 +0200

    added files

---
+++ advanced/script.ts
@@ -0,0 +1,55 @@
+type MenuItem = {
+    name: string;
+    description: string;
+    price: number;
+};
+
+const menu: MenuItem[] = [
+    {
+        name: "Spaghetti Bolognese (vegan)",
+        description: "Delicious spaghetti with a rich vegan soy-chunks sauce.",
+        price: 12.99
+    },
+    {
+        name: "Margherita Pizza (vegan)",
+        description: "Classic margherita pizza with tomato and vegan mozzarella.",
+        price: 10.99
+    },
+    {
+        name: "Caesar Salad (vegan)",
+        description: "Fresh salad with romaine lettuce, croutons, and Caesar dressing.",
+        price: 8.99
+    },    
+    {
+        name: "Chana Masala (vegan)",
+        description: "Indian spicy chickpeas curry.",
+        price: 11.99
+    },
+];
+
+function populateMenu() {
+    const menuList = document.getElementById("menu-list");
+
+    if (menuList) {
+        menu.forEach((item) => {
+            const menuItemElement = document.createElement("div");
+            menuItemElement.className = "row";
+            menuItemElement.innerHTML = `
+                <div class="col-md-6"><h6>${item.name}</h6></div>
+                <div class="col-md-6 d-flex justify-content-end">${item.price.toFixed(2)}</div><br>
+                <div class="col-md-12 fs-6 fst-italic">${item.description}</div>
+
+            `;
+
+            const listItem = document.createElement("li");
+            listItem.className = "list-group-item  list-group-item-light";
+            listItem.appendChild(menuItemElement);
+
+            menuList.appendChild(listItem);
+        });
+    }
+}
+
+populateMenu();
+
+
---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+// Basic exercise 1
+let tableSize = 10;
+for (let i = 1; i <= tableSize; i++) {
+    for (let j = 1; j <= tableSize; j++) {
+        const product = i * j;
+        console.log(`${j} x ${i} = ${product}\n`);
+    }
+    console.log(`\n`);
+}
+// Basic exercise 2 - 1.
+let fullName = {
+    fName: 'Chris',
+    lName: 'Runda'
+};
+// Access the properties and print the full name to the browser
+document.write(`Full Name: ${fullName.fName} ${fullName.lName}<br><br>`);
+// Basic exercise 2 - 2.
+let fullNameArray = Array(10).fill(fullName);
+fullNameArray.forEach((nameObject, index) => {
+    document.write(`<br>First Name #${++index}: ${nameObject.fName}`);
+});
+document.write(`<br><br>`);
+// Basic exercise 2 - 3.
+function printLastName() {
+    for (let i = 0; i < 10; i++) {
+        console.log(`${i + 1}: ${fullName.lName}`);
+    }
+}
+setTimeout(printLastName, 5000);
+//   Basic exercise 3
+const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// 1. Print only the index of the elements
+console.log("Indices:");
+names.forEach((name, index) => {
+    console.log(index);
+});
+// 2. Print only the values
+console.log("\nValues:");
+names.forEach((name) => {
+    console.log(name);
+});
+// 3. Print both index and values
+console.log("\nIndices and Values:");
+names.forEach((name, index) => {
+    console.log(`Index: ${index}, Value: ${name}`);
+});
